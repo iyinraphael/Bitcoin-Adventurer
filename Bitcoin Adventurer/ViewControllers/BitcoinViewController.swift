726939ca836eccd440b3e6f4f1e108760e61dd80
iyinraphael <iyinraphael@gmail.com>
2021-09-29 21:42:15 -0400

    dependency injection

--- Bitcoin Adventurer/ViewControllers/BitcoinViewController.swift
+++ Bitcoin Adventurer/ViewControllers/BitcoinViewController.swift
@@ -34,6 +34,8 @@ internal class BitcoinViewController: UIViewController {
   @IBOutlet weak private var primary: UILabel!
   @IBOutlet weak private var partial: UILabel!
   
+  let fetcher = BitcoinPriceFetcher(networking: HTTPNetworking())
+
   private let dollarsDisplayFormatter: NumberFormatter = {
     let formatter = NumberFormatter()
     formatter.maximumFractionDigits = 0
@@ -66,38 +68,13 @@ internal class BitcoinViewController: UIViewController {
     partial.text = ".\(cents)"
   }
   
-  private func requestPrice()  {
-    let bitcoin = Coinbase.bitcoin.path
-    
-    // 1. Make URL request
-    guard let url = URL(string: bitcoin) else { return }
-    var request = URLRequest(url: url)
-    request.cachePolicy = .reloadIgnoringCacheData
-    
-    // 2. Make networking request
-    let task = URLSession.shared.dataTask(with: request) { data, _, error in
-      
-      // 3. Check for errors
-      if let error = error {
-        print("Error received requesting Bitcoin price: \(error.localizedDescription)")
-        return
-      }
-      
-      // 4. Parse the returned information
-      let decoder = JSONDecoder()
+  private func requestPrice() {
+    fetcher.fetch { response in
+      guard let response = response else { return }
 
-      guard let data = data,
-            let response = try? decoder.decode(PriceResponse.self,
-                                               from: data) else { return }
-      
-      print("Price returned: \(response.data.amount)")
-      
-      // 5. Update the UI with the parsed PriceResponse
       DispatchQueue.main.async { [weak self] in
         self?.updateLabel(price: response.data)
       }
     }
-
-    task.resume()
   }
 }
